aacdd8d8eca937bb8c77e3689ea0fc80ce7a9a0f
thorsjur <thor.sjursen@ntnu.no>
2023-10-09 12:08:51 +0200

    Added header with navbar (#4)

--- client/src/pages/root/Root.tsx
+++ client/src/pages/root/Root.tsx
@@ -1,5 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from '../../components/header/header';
 
 export const Root = () => {
-    return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 };
