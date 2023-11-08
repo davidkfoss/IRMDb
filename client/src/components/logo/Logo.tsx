3d869090b26e452b78c304bdebcf2aacd1b23de7
Christian Veiby <chriveib@stud.ntnu.no>
2023-11-08 12:10:20 +0100

    Add hover effect and link to homepage on logo #28

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import './Logo.css';
 
 export const Logo = () => {
+  const navigate = useNavigate();
   return (
     <div className='logo-container'>
-      <div className='logo-box'>
+      <div className={'logo-box'} onClick={() => navigate('/')}>
         <span className='logo-text'>IRMDb</span>
       </div>
     </div>
