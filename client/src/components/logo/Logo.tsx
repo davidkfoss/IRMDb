7776cd22e49a56d82cc217a2154973cdcd4a3169
Christian Veiby <chriveib@stud.ntnu.no>
2023-11-08 11:07:03 +0100

    Small fix with db connection #28

---
+++ client/src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import './Logo.css';
+
+export const Logo = () => {
+  return (
+    <div className='logo-container'>
+      <div className='logo-box'>
+        <span className='logo-text'>irmdb</span>
+      </div>
+    </div>
+  );
+};
